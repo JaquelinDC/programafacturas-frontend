3cbee32373aae6e202096d32c7f2866ed3e35f1d
JaquelinDC <107244356+JaquelinDC@users.noreply.github.com>
2026-06-02 12:14:24 +0200

    adasd

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,19 @@ export const $api = ofetch.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   async onRequest({ options }) {
     const accessToken = useCookie('accessToken').value
-    if (accessToken)
-      options.headers.append('Authorization', `Bearer ${accessToken}`)
+    if (accessToken) {
+      options.headers = {
+        ...(options.headers as unknown as Record<string, string>),
+        Authorization: `Bearer ${accessToken}`,
+      }
+    }
+  },
+  async onResponseError({ response }) {
+    if (response.status === 401) {
+      const accessToken = useCookie<string | null>('accessToken')
+      accessToken.value = null
+      const router = useRouter()
+      router.push('/login')
+    }
   },
 })
